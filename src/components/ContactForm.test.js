a933088c6d7bc3c51437de61582719f75d979c01
leooladimu <82811737+leooladimu@users.noreply.github.com>
2021-10-28 21:36:11 -0400

    1

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -4,12 +4,14 @@ import userEvent from '@testing-library/user-event';
 
 import ContactForm from './ContactForm';
 
-test('renders without errors', ()=>{
-    
+test('renders without errors', () => {
+    render(<ContactForm/>);
 });
 
-test('renders the contact form header', ()=> {
-    
+test('renders the contact form header', () => {
+    render(<ContactForm/>);
+    const headerElement = screen.queryByText(/Contact Form/i);
+    console.log(headerElement);
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
